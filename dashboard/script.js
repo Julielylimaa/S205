7ffbfcbc70cea044296880f05ac9c9851c752afe
Juliely <juhlima26@icloud.com>
2025-03-10 17:04:57 -0300

    feat: :sparkles: add theme

---
+++ dashboard/script.js
@@ -0,0 +1,9 @@
+function toggleThemeMenu() {
+  let menu = document.getElementById("themeMenu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+function setTheme(theme) {
+  document.body.className = theme;
+  document.getElementById("themeMenu").style.display = "none";
+}
